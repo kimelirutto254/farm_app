252bbe5157add0a56fa6f2abcbcca32c3a9564b8
dismas <kimzdismasrutto@gmail.com>
2025-03-06 08:43:15 +0300

    Fix Flutter root issue

--- vercel_build.sh
+++ vercel_build.sh
@@ -5,9 +5,14 @@ set -e
 
 echo "Installing Flutter..."
 
-# Install Flutter
+# Clone Flutter repository
 git clone https://github.com/flutter/flutter.git -b stable --depth 1
 export PATH="$PWD/flutter/bin:$PATH"
+
+# Disable root user check
+export FLUTTER_SUPPRESS_ANALYTICS=true
+export NO_COLOR=true
+export CI=true
 
 echo "Flutter installed successfully."
 
@@ -17,7 +22,7 @@ flutter config --enable-web
 # Get dependencies
 flutter pub get
 
-# Build Flutter web app
+# Build the Flutter web app
 flutter build web
 
 echo "Flutter web build complete."
